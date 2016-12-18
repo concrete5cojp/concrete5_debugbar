2e5a823dba4c983f84e78f568d74533e35158570
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2016-12-19 03:08:43 +0900

    Enables to add messages/measures

--- controller.php
+++ controller.php
@@ -2,8 +2,6 @@
 namespace Concrete\Package\Debugbar;
 
 use Concrete\Core\Package\Package;
-use Concrete\Core\View\View;
-use Concrete5Debugbar\Debugbar;
 use Database;
 use Events;
 
@@ -15,6 +13,8 @@ class Controller extends Package
     protected $pkgAutoloaderRegistries = [
         'src/Concrete5Debugbar' => 'Concrete5Debugbar'
     ];
+
+    const PLACEHOLDER_TEXT = '<!-- debugbar:placeholder -->';
 
     /**
      * Returns the translated name of the package.
@@ -40,15 +40,34 @@ class Controller extends Package
     {
         require $this->getPackagePath() . '/vendor/autoload.php';
 
-        Events::addListener('on_before_render', function ($event) {
-            $debugbar = new Debugbar();
-            $debugbarRenderer = $debugbar->getJavascriptRenderer(
+        $this->app->singleton('debugbar', 'Concrete5Debugbar\Debugbar');
+        $this->app->bind('debugbar/renderer', function() {
+            $debugbar = $this->app->make('debugbar');
+            return $debugbar->getJavascriptRenderer(
                 $this->getRelativePath() . '/vendor/maximebf/debugbar/src/DebugBar/Resources'
             );
+        });
+        $this->app->bind('debugbar/messages', function() {
+            $debugbar = $this->app->make('debugbar');
+            return $debugbar['messages'];
+        });
+        $this->app->bind('debugbar/time', function() {
+            $debugbar = $this->app->make('debugbar');
+            return $debugbar['time'];
+        });
 
+        Events::addListener('on_before_render', function ($event) {
+            $debugbarRenderer = $this->app->make('debugbar/renderer');
             $v = $event->getArgument('view');
             $v->addHeaderItem($debugbarRenderer->renderHead());
-            $v->addFooterItem($debugbarRenderer->render());
+            $v->addFooterItem(self::PLACEHOLDER_TEXT);
+        });
+
+        Events::addListener('on_page_output', function ($event) {
+            $debugbarRenderer = $this->app->make('debugbar/renderer');
+            $contents = $event->getArgument('contents');
+            $contents = str_replace(self::PLACEHOLDER_TEXT, $debugbarRenderer->render(), $contents);
+            $event->setArgument('contents', $contents);
         });
     }
 }
