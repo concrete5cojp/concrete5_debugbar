1da7df814ad137e32a76d31e4cb3bffdbd486df8
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2016-12-19 03:37:30 +0900

    Add SQL Collector

--- src/Concrete5Debugbar/Debugbar.php
+++ src/Concrete5Debugbar/Debugbar.php
@@ -1,10 +1,12 @@
 <?php
 namespace Concrete5Debugbar;
 
+use DebugBar\Bridge\DoctrineCollector;
 use DebugBar\DataCollector\MemoryCollector;
 use DebugBar\DataCollector\MessagesCollector;
 use DebugBar\DataCollector\PhpInfoCollector;
 use DebugBar\DataCollector\TimeDataCollector;
+use Doctrine\DBAL\Logging\DebugStack;
 
 class Debugbar extends \DebugBar\DebugBar
 {
@@ -17,9 +19,11 @@ class Debugbar extends \DebugBar\DebugBar
         $this->addCollector(new MessagesCollector());
         $this->addCollector(new TimeDataCollector());
         $this->addCollector(new MemoryCollector());
+        $doctrineDebugStack = new DebugStack();
+        \Core::make('Concrete\Core\Database\DatabaseManager')->getConfiguration()->setSQLLogger($doctrineDebugStack);
+        $this->addCollector(new DoctrineCollector($doctrineDebugStack));
         // TODO: Concrete5AuthenticationCollector
         // TODO: Concrete5ControllerCollector
-        // TODO: Concrete5DatabaseQueryCollector
         // TODO: Concrete5LoggingCollector
         // TODO: Concrete5RequestCollector
         // TODO: Concrete5RouteCollector
